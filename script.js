0caae8481c4f7d3b5306c364285cc73de7016f2e
Sebastian S <sebastiansiewweiqi@gmail.com>
2023-02-11 00:23:43 +0800

    Added Random Rock Paper Scissors function

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3);
+
+    if(x === 0)
+        console.log('Rock');
+
+    else if(x === 1)
+        console.log('Paper');
+
+    else
+        console.log('Scissors');
+}
+
+function playRound(playerSelection, computerSelection){
     
 }
+
+getComputerChoice();
