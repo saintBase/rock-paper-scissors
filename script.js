b2361e515725a59c5862e8fc3b980b20fcccb0a3
Sebastian S <sebastiansiewweiqi@gmail.com>
2023-02-11 01:01:40 +0800

    Added playing rules function

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 function getComputerChoice() {
-    let x = Math.floor(Math.random() * 3);
+  let x = Math.floor(Math.random() * 3);
 
-    if(x === 0)
-        console.log('Rock');
-
-    else if(x === 1)
-        console.log('Paper');
-
-    else
-        console.log('Scissors');
+  if (x === 0) return "Rock";
+  else if (x === 1) return "Paper";
+  else return "Scissors";
 }
 
-function playRound(playerSelection, computerSelection){
-    
+function playRound(playerSelection, computerSelection) {
+  let result = "";
+  playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substr(1).toLowerCase();
+  if (playerSelection === "Rock") {
+    if (computerSelection === "Paper")
+      result = "You Lose! " + computerSelection + " beats " + playerSelection;
+    else if (computerSelection === "Scissors")
+      result = "You Win! " + playerSelection + " beats " + computerSelection;
+    else result = "Draw!";
+  } else if (playerSelection === "Paper") {
+    if (computerSelection === "Scissors")
+      result = "You Lose! " + computerSelection + " beats " + playerSelection;
+    else if (computerSelection === "Rock")
+      result = "You Win! " + playerSelection + " beats " + computerSelection;
+    else result = "Draw!";
+  } else if (playerSelection === "Scissors") {
+    if (computerSelection === "Rock")
+      result = "You Lose! " + computerSelection + " beats " + playerSelection;
+    else if (computerSelection === "Paper")
+      result = "You Win! " + playerSelection + " beats " + computerSelection;
+    else result = "Draw!";
+  }
+  console.log(playerSelection + " " + computerSelection);
+  return result;
 }
 
-getComputerChoice();
+function game(){
+    let counter = 0;
+    for(let i=0; i < 5; i++){
+        let game = playRound();
+        if (game.includes('Win'))
+            counter++;
+    }
+}
+
+const playerSelection = prompt("Rock, Paper or Scissors?");
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
