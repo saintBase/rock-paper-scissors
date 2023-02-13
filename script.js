0997e9b7e0c53c601c61d95f4c0614aff7c72225
Sebastian S <sebastiansiewweiqi@gmail.com>
2023-02-14 00:20:16 +0800

    Added a basic UI to display results from rps

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore, comScore;
+playerScore = comScore = 0;
+
 function getComputerChoice() {
   let x = Math.floor(Math.random() * 3);
 
@@ -8,7 +11,9 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   let result = "";
-  playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substr(1).toLowerCase();
+  playerSelection =
+    playerSelection.substring(0, 1).toUpperCase() +
+    playerSelection.substr(1).toLowerCase();
   if (playerSelection === "Rock") {
     if (computerSelection === "Paper")
       result = "You Lose! " + computerSelection + " beats " + playerSelection;
@@ -27,15 +32,29 @@ function playRound(playerSelection, computerSelection) {
     else if (computerSelection === "Paper")
       result = "You Win! " + playerSelection + " beats " + computerSelection;
     else result = "Draw!";
+  } else {
+    console.log("You have entered an invalid input!");
   }
-  else {
-    console.log("You have entered an invalid input!")
-  }
-  console.log("Player: " + playerSelection + " Computer : " + computerSelection);
-  return result;
+  console.log(
+    "Player: " + playerSelection + " Computer : " + computerSelection
+  );
+  //return result;
+  // write result and append to div DOM
+  resultDOM = document.createElement("p");
+  resultDOM.textContent = result;
+  resultDiv.appendChild(resultDOM);
+  if (result.includes("Win")) playerScore++;
+  else if (result.includes("Lose")) comScore++;
+
+  document.getElementById('scoreText').textContent = `Player: ${playerScore} Computer: ${comScore}`;
+
+  if(playerScore == 5)
+    alert("You WIN!");
+  else if(comScore == 5)
+    alert("You LOSE!");
 }
 
-function game(){
+/*function game(){
     let player = 0;
     let computer = 0;
     for(let i= 0; i < 5; i++){
@@ -55,8 +74,37 @@ function game(){
     else
         return "No Winners, no Losers!"
 }
-
-
+*/
 
 //console.log(playRound(playerSelection, computerSelection));
-console.log(game());
+//console.log(game());
+
+const rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener("click", function () {
+  playRound(rockBtn.textContent, getComputerChoice());
+});
+const paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener("click", function () {
+  playRound(paperBtn.textContent, getComputerChoice());
+});
+const scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.addEventListener("click", function () {
+  playRound(scissorsBtn.textContent, getComputerChoice());
+});
+
+//creatning result DOM
+const resultDiv = document.querySelector("#result");
+
+// creating score DOM
+const scoreDiv = document.querySelector("#score");
+let initScore = document.createElement("h2");
+initScore.textContent = `Player: ${playerScore} Computer: ${comScore}`;
+initScore.setAttribute("id", "scoreText");
+scoreDiv.appendChild(initScore);
+
+window.addEventListener('load', function(){
+  if(playerScore == 5)
+    this.alert("You WIN!");
+  else if(comScore == 5)
+    this.alert("You LOSE!");
+});
